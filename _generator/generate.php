d367aebcca7187fa6459e157a1f75c09da90b2db
Skayo <jonasg.cool@gmail.com>
2019-08-31 15:57:50 +0200

    Various Changes

--- _generator/generate.php
+++ _generator/generate.php
@@ -2,20 +2,11 @@
 
 echo PHP_EOL . PHP_EOL . '<< SkayoCrafts Website - Static Site Generator >>' . PHP_EOL . PHP_EOL;
 
-echo '> Reading _plugins.json... ';
+echo '> Reading _plugins.xml... ';
 
-$rawJson = file_get_contents('_plugins.json');
+$plugins = simplexml_load_file('_plugins.xml');
 
-if (!$rawJson)
-	die('Failed!' . PHP_EOL);
-
-echo 'Done.' . PHP_EOL;
-
-echo '> Decoding JSON... ';
-
-$plugins = json_decode($rawJson, true);
-
-if (is_null($plugins))
+if (!$plugins)
 	die('Failed!' . PHP_EOL);
 
 echo 'Done.' . PHP_EOL;
@@ -24,10 +15,10 @@ echo '> Making plugin list for _templates/_index.html... ';
 
 $pluginListHtml = '';
 
-foreach ($plugins as $plugin) {
+foreach ($plugins->children() as $plugin) {
 	$pluginListHtml .=
 		'<li>' .
-		"\t<a title=\"Go to plugin page\" href=\"/{$plugin['handle']}\" target=\"_blank\">{$plugin['name']}</a>" .
+		"<a class=\"plugin-link\" title=\"Go to plugin page\" href=\"/{$plugin->handle}\">{$plugin->name}</a>" .
 		'</li>';
 }
 
@@ -40,15 +31,15 @@ $indexHtmlTemplate = file_get_contents('_templates/_index.html');
 if (!$indexHtmlTemplate)
 	die('Failed!' . PHP_EOL);
 
-$indexHtml = preg_replace('/\{\{ *plugin_list *\}\}/', $pluginListHtml, $indexHtmlTemplate);
+$indexHtml = preg_replace('/\{\{ *plugin-list *\}\}/', $pluginListHtml, $indexHtmlTemplate);
 
 if (!file_put_contents('index.html', $indexHtml))
 	die('Failed!' . PHP_EOL);
 
 echo 'Done.' . PHP_EOL;
 
-foreach ($plugins as $plugin) {
-	echo "> Generating {$plugin['handle']}/index.html out of _templates/_plugin.html... ";
+foreach ($plugins->children() as $plugin) {
+	echo "> Generating {$plugin->handle}/index.html out of _templates/_plugin.html... ";
 
 	$pluginHtmlTemplate = file_get_contents('_templates/_plugin.html');
 
@@ -57,14 +48,14 @@ foreach ($plugins as $plugin) {
 
 	$pluginHtml = $pluginHtmlTemplate;
 
-	foreach (array_keys($plugin) as $key) {
-		$pluginHtml = preg_replace('/\{\{ *' . $key . ' *\}\}/', $plugin[$key], $pluginHtml);
+	foreach (array_keys(get_object_vars($plugin)) as $key) {
+		$pluginHtml = preg_replace('/\{\{ *' . $key . ' *\}\}/', $plugin->{$key}, $pluginHtml);
 	}
 
-	if (!is_dir($plugin['handle']) && !mkdir($plugin['handle']))
+	if (!is_dir($plugin->handle) && !mkdir($plugin->handle))
 		die('Failed!' . PHP_EOL);
 
-	if (!file_put_contents("{$plugin['handle']}/index.html", $pluginHtml))
+	if (!file_put_contents("{$plugin->handle}/index.html", $pluginHtml))
 		die('Failed!' . PHP_EOL);
 
 	echo 'Done.' . PHP_EOL;
